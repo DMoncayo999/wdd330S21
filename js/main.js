21d9d62aee9d32dfa0a97ce5ddd25c375e1d7bda
DELFINA MONCAYO <mon19012@byui.edu>
2021-04-24 22:00:20 -0400

    js dynamic menu content

---
+++ js/main.js
@@ -0,0 +1,32 @@
+//  Dynamic Navigation
+
+const links = [
+      {
+        label: "Week1",
+        url: "week01/index.html"
+      },
+      {
+        label: "Week2",
+        url: "week02/index.html"
+      },
+      {
+        label: "Challenge1: Todo",
+        url: "challenge1/index.html"
+      }
+]
+
+const nav = ( items ) => {
+  
+  let menu = document.querySelector('#menu') // ol
+  
+  items.forEach( (item, inx, arr) => {
+      let l = document.createElement('li')
+      let a = document.createElement('a')
+      a.href = item.url 
+      a.innerHTML = item.label
+      l.appendChild(a)
+      menu.appendChild(l)
+  })
+}
+
+nav( links )
