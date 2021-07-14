aeb0811051c94265248c8c82c06a02e9055e7ea4
Ocm2 <ocm2@gmail.com>
2021-07-14 01:08:26 -0400

    Updating Calendar and Agenda

--- myagenda/main.js
+++ myagenda/main.js
@@ -9,42 +9,60 @@ Date.prototype.addMonths = function(months) {
       return this
 }
 
-/*
-const urlParams = window.location.replaceAll(/([^&=]+)=?([^&]*)/g, (m, k , v) => {
-    console.log("m", m, "k", k, "v", v)
-})
-*/
-var now = new Date()
-var req = { query: {} }
-var year = req.query.year || 1900 + now.getYear()
-var month = req.query.month || now.getMonth()
-var day = req.query.day || now.getDate()
+const calendario = new Date()
 
-console.log('year', year, 'month', month, 'day', day)
-now = new Date( year, month, day )
-var cal = new Date( year, month, 1)
-cal.addDays(-1 * ( cal.getDay()+1 ) )
-  
-const calendar = new Date()
 
-document.querySelector(".agenda h1").innerHTML = calendar.toDateString()
-    
-document.querySelectorAll(".calendar span").forEach( (e,i) => { 
-    calendar.addMonths(i)
-    e.innerHTML = calendar.toLocaleString('default', { month: 'long'})
-})
+const showCalendar = (calendar) => {
 
-var calendarArray = ['<div class="weekday">S</div>', 
-            '<div class="weekday">M</div>',
-            '<div class="weekday">T</div>',
-            '<div class="weekday">W</div>',
-            '<div class="weekday">T</div>',
-            '<div class="weekday">F</div>',
-            '<div class="weekday">S</div>' ]
-            
-for (let i = 1; i < 43; i++) {
-    calendarArray.push( '<a href="#" class="day" >' + `${ cal.addDays(1).getDate() }` + '</a>' )
+  document.querySelector(".agenda h1").innerHTML = calendar.toDateString()
+  var now = new Date()
+  var req = { query: {} }
+  var year = req.query.year || 1900 + calendar.getYear()
+  var month = req.query.month || calendar.getMonth()
+  var day = req.query.day || calendar.getDate()
+
+  console.log('year', year, 'month', month, 'day', day)
+  var cal = new Date(year, month, 1 )
+  cal.addDays(-1 * ( cal.getDay()+1 ) )
+  var calendarArray = ['<div class="weekday">S</div>',
+              '<div class="weekday">M</div>',
+              '<div class="weekday">T</div>',
+              '<div class="weekday">W</div>',
+              '<div class="weekday">T</div>',
+              '<div class="weekday">F</div>',
+              '<div class="weekday">S</div>' ]
+
+  for (let i = 1; i < 43; i++) {
+      cal.addDays(1)
+      calendarArray.push( '<a href="#" ' + ( now.getMonth() !== cal.getMonth() ? ' class="noMonth" ' : ( now.getDate() === cal.getDate() ? 'class="today"' :'class="day"' ) ) + ' onclick="onDayOfTheMonth(event)" >' + `${ cal.getDate() }` + '</a>' )
+  }
+
+
+  document.querySelectorAll(".cal-header .monthName").forEach( (e,i) => {
+      calendar.addMonths(i)
+      e.innerHTML = calendar.toLocaleString('default', { month: 'long'}) + ' - ' + calendar.toLocaleString('default', { year: 'numeric'})
+  })
+  document.querySelector('.date').innerHTML = calendarArray.join('')
 }
-document.querySelector('.date').innerHTML = calendarArray.join('')
 
+const prevBtn = () => {
+  calendario.addMonths(-1)
+  showCalendar(calendario)
+}
+const nextBtn = () => {
+  calendario.addMonths(1)
+  showCalendar(calendario)
+}
 
+const onDayOfTheMonth = (event) => {
+  // debugger
+  let d = parseInt( event.currentTarget.innerText )
+  calendario.setDate( d )
+  if ( event.currentTarget.className === 'noMonth' ) {
+     if (d<14) calendario.addMonths(1)
+     else calendario.addMonths(-1)
+  }
+  showCalendar(calendario)
+}
+
+showCalendar(calendario)
